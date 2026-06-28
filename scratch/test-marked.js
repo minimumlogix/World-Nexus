0e0538d53df9ca6b79ab8f6885add1a886567fd7
minimumlogix <minimumlogix@gmail.com>
2026-06-28 20:38:08 +0530

    refactor: remove legacy HTML files and add new character concept assets

--- scratch/test-marked.js
+++
@@ -1,9 +0,0 @@
-import { marked } from '../js/lib/marked.esm.js';
-
-console.log('marked:', typeof marked);
-console.log('marked.parse:', typeof marked.parse);
-console.log('marked.parseInline:', typeof marked.parseInline);
-
-const testString = 'This is *italic* and [link](http://example.com)';
-console.log('parse:', marked.parse(testString));
-console.log('parseInline:', marked.parseInline(testString));
